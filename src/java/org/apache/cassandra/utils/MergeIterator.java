2b53553a4af3b2e293a8878faeaeb21ca71b0d66
Branimir Lambov <branimir.lambov@datastax.com>
2015-03-04 20:15:59 +0000

    Implements more efficient MergeIterator using replaceTop operation in a binary heap with sorted initial section.

--- src/java/org/apache/cassandra/utils/MergeIterator.java
+++ src/java/org/apache/cassandra/utils/MergeIterator.java
@@ -73,57 +73,115 @@ public abstract class MergeIterator<In,Out> extends AbstractIterator<Out> implem
     /** A MergeIterator that consumes multiple input values per output value. */
     private static final class ManyToOne<In,Out> extends MergeIterator<In,Out>
     {
-        // a queue for return: all candidates must be open and have at least one item
-        protected final PriorityQueue<Candidate<In>> queue;
-        // a stack of the last consumed candidates, so that we can lazily call 'advance()'
-        // TODO: if we had our own PriorityQueue implementation we could stash items
-        // at the end of its array, so we wouldn't need this storage
-        protected final ArrayDeque<Candidate<In>> candidates;
+        protected final Candidate<In>[] heap;
+        int size;
+
         public ManyToOne(List<? extends Iterator<In>> iters, Comparator<In> comp, Reducer<In, Out> reducer)
         {
             super(iters, reducer);
-            this.queue = new PriorityQueue<>(Math.max(1, iters.size()));
+
+            @SuppressWarnings("unchecked")
+            Candidate<In>[] heap = new Candidate[iters.size()];
+            this.heap = heap;
+            size = 0;
             for (Iterator<In> iter : iters)
             {
-                Candidate<In> candidate = new Candidate<>(iter, comp);
-                if (!candidate.advance())
-                    // was empty
-                    continue;
-                this.queue.add(candidate);
+                Candidate<In> candidate = new Candidate<>(iter, comp).advance();
+                if (candidate != null)
+                    heap[size++] = candidate;
             }
-            this.candidates = new ArrayDeque<>(queue.size());
+            Arrays.sort(heap, 0, size);
         }
 
         protected final Out computeNext()
         {
-            advance();
-            return consume();
-        }
-
-        /** Consume values by sending them to the reducer while they are equal. */
-        protected final Out consume()
-        {
             reducer.onKeyChange();
-            Candidate<In> candidate = queue.peek();
-            if (candidate == null)
+            if (size == 0)
                 return endOfData();
+
+            In item = heap[0].item;
             do
             {
-                candidate = queue.poll();
-                candidates.push(candidate);
+                Candidate<In> candidate = heap[0];
                 reducer.reduce(candidate.item);
+                replaceTop(candidate.advance());
             }
-            while (queue.peek() != null && queue.peek().compareTo(candidate) == 0);
+            while (size > 0 && heap[0].equalItem(item));
             return reducer.getReduced();
         }
 
-        /** Advance and re-enqueue all items we consumed in the last iteration. */
-        protected final void advance()
+        /**
+         * The number of elements to keep in order before the binary heap starts, exclusive of the top heap element.
+         * This section helps the performance in cases where iterators are largely non-overlapping.
+         * 
+         * The heap is laid out as (example for SORTED_SECTION == 2):
+         *                 0
+         *                 |
+         *                 1
+         *                 |
+         *                 2
+         *               /   \
+         *              3     4
+         *             / \   / \
+         *             5 6   7 8
+         *            .. .. .. ..
+         * Where each line is a <= relationship. In the sorted section we can advance with a single comparison per level,
+         * while advancing a level within the heap requires two (so that we can find the lighter element to pop up).
+         * The sorted section adds a constant overhead when data is uniformly distributed among the iterators, but may up
+         * to halve the iteration time when one iterator is dominant over sections of the merged data (as is the case with
+         * non-overlapping iterators).
+         */
+        static final int SORTED_SECTION = 2;
+
+        /**
+         * Replace the top element with the given and move it down in the heap until it finds its proper position.
+         */
+        private void replaceTop(Candidate<In> candidate)
         {
-            Candidate<In> candidate;
-            while ((candidate = candidates.pollFirst()) != null)
-                if (candidate.advance())
-                    queue.add(candidate);
+            if (candidate == null)
+            {
+                candidate = heap[--size];
+                heap[size] = null; // not necessary but helpful for debugging
+            }
+
+            final int size = this.size;
+            int currIdx = 0;
+            final int sortedSectionSize = Math.min(size, SORTED_SECTION + 1);
+            int nextIdx = currIdx + 1;
+
+            loop:
+            {
+                while (nextIdx < sortedSectionSize)
+                {
+                    if (candidate.compareTo(heap[nextIdx]) <= 0)
+                    {
+                        break loop;
+                    }
+
+                    heap[currIdx] = heap[nextIdx];
+                    currIdx = nextIdx;
+                    nextIdx = currIdx + 1;
+                }
+
+                while (nextIdx < size)
+                {
+                    if ((nextIdx + 1 < size)
+                            && heap[nextIdx + 1].compareTo(heap[nextIdx]) < 0)
+                    {
+                        ++nextIdx;
+                    }
+
+                    if (candidate.compareTo(heap[nextIdx]) <= 0)
+                    {
+                        break loop;
+                    }
+
+                    heap[currIdx] = heap[nextIdx];
+                    currIdx = nextIdx;
+                    nextIdx = (nextIdx << 1) - (SORTED_SECTION - 1);
+                }
+            }
+            heap[currIdx] = candidate;
         }
     }
 
@@ -132,7 +190,7 @@ public abstract class MergeIterator<In,Out> extends AbstractIterator<Out> implem
     {
         private final Iterator<In> iter;
         private final Comparator<In> comp;
-        private In item;
+        In item;
 
         public Candidate(Iterator<In> iter, Comparator<In> comp)
         {
@@ -140,18 +198,23 @@ public abstract class MergeIterator<In,Out> extends AbstractIterator<Out> implem
             this.comp = comp;
         }
 
-        /** @return True if our iterator had an item, and it is now available */
-        protected boolean advance()
+        /** @return this if our iterator had an item, and it is now available, otherwise null */
+        protected Candidate<In> advance()
         {
             if (!iter.hasNext())
-                return false;
+                return null;
             item = iter.next();
-            return true;
+            return this;
         }
 
         public int compareTo(Candidate<In> that)
         {
             return comp.compare(this.item, that.item);
+        }
+
+        public boolean equalItem(In thatItem)
+        {
+            return this.item.equals(thatItem);
         }
     }
 
---
+++ test/unit/org/apache/cassandra/utils/MergeIteratorLongTest.java
@@ -0,0 +1,399 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.apache.cassandra.utils;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.concurrent.Callable;
+
+import com.google.common.base.Function;
+import com.google.common.base.Objects;
+import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.db.marshal.TimeUUIDType;
+import org.apache.cassandra.db.marshal.UUIDType;
+import org.apache.cassandra.utils.MergeIterator.Reducer;
+
+public class MergeIteratorLongTest
+{
+    static int ITERATOR_COUNT = 15;
+    static int LIST_LENGTH = 200000;
+    static boolean BENCHMARK = true;//false;
+    
+    @Test
+    public void testRandomInts() throws Exception
+    {
+        final Random r = new Random();
+        Comparator<Integer> comparator = Ordering.natural();
+        Reducer<Integer, Counted<Integer>> reducer = new Counter<Integer>();
+
+        List<List<Integer>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception
+            {
+                return r.nextInt(5 * LIST_LENGTH);
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+    
+    @Test
+    public void testNonOverlapInts() throws Exception
+    {
+        Comparator<Integer> comparator = Ordering.natural();
+        Reducer<Integer, Counted<Integer>> reducer = new Counter<Integer>();
+
+        List<List<Integer>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<Integer>() {
+            int next = 1;
+            @Override
+            public Integer call() throws Exception
+            {
+                return next++;
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    @Test
+    public void testCombinationInts() throws Exception
+    {
+        final Random r = new Random();
+        Comparator<Integer> comparator = Ordering.natural();
+        Reducer<Integer, Counted<Integer>> reducer = new Counter<Integer>();
+
+        List<List<Integer>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<Integer>() {
+            int next = 1;
+            @Override
+            public Integer call() throws Exception
+            {
+                return r.nextBoolean() ? r.nextInt(5 * LIST_LENGTH) : next++;
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+    
+    @Test
+    public void testRandomStrings() throws Exception
+    {
+        final Random r = new Random();
+        Comparator<String> comparator = Ordering.natural();
+        Reducer<String, Counted<String>> reducer = new Counter<String>();
+
+        List<List<String>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<String>() {
+            @Override
+            public String call() throws Exception
+            {
+                return "longish_prefix_" + r.nextInt(5 * LIST_LENGTH);
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+    
+    @Test
+    public void testNonOverlapStrings() throws Exception
+    {
+        Comparator<String> comparator = Ordering.natural();
+        Reducer<String, Counted<String>> reducer = new Counter<String>();
+
+        List<List<String>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<String>() {
+            int next = 1;
+            @Override
+            public String call() throws Exception
+            {
+                return "longish_prefix_" + next++;
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    @Test
+    public void testCombinationStrings() throws Exception
+    {
+        final Random r = new Random();
+        Comparator<String> comparator = Ordering.natural();
+        Reducer<String, Counted<String>> reducer = new Counter<String>();
+
+        List<List<String>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<String>() {
+            int next = 1;
+            @Override
+            public String call() throws Exception
+            {
+                return "longish_prefix_" + (r.nextBoolean() ? r.nextInt(5 * LIST_LENGTH) : next++);
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    @Test
+    public void testTimeUuids() throws Exception
+    {
+        Comparator<UUID> comparator = Ordering.natural();
+        Reducer<UUID, Counted<UUID>> reducer = new Counter<UUID>();
+
+        List<List<UUID>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<UUID>() {
+            @Override
+            public UUID call() throws Exception
+            {
+                return UUIDGen.getTimeUUID();
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    @Test
+    public void testRandomUuids() throws Exception
+    {
+        Comparator<UUID> comparator = Ordering.natural();
+        Reducer<UUID, Counted<UUID>> reducer = new Counter<UUID>();
+
+        List<List<UUID>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<UUID>() {
+            @Override
+            public UUID call() throws Exception
+            {
+                return UUID.randomUUID();
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    @Test
+    public void testTimeUuidType() throws Exception
+    {
+        AbstractType<UUID> comparator = TimeUUIDType.instance;
+        Reducer<ByteBuffer, Counted<ByteBuffer>> reducer = new Counter<ByteBuffer>();
+
+        List<List<ByteBuffer>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<ByteBuffer>() {
+            @Override
+            public ByteBuffer call() throws Exception
+            {
+                return comparator.decompose(UUIDGen.getTimeUUID());
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    @Test
+    public void testUuidType() throws Exception
+    {
+        AbstractType<UUID> comparator = UUIDType.instance;
+        Reducer<ByteBuffer, Counted<ByteBuffer>> reducer = new Counter<ByteBuffer>();
+
+        List<List<ByteBuffer>> lists = generateLists(ITERATOR_COUNT, LIST_LENGTH, new Callable<ByteBuffer>() {
+            @Override
+            public ByteBuffer call() throws Exception
+            {
+                return comparator.decompose(UUID.randomUUID());
+            }
+        }, comparator);
+        testMergeIterator(comparator, reducer, lists);
+    }
+
+    public <T> List<List<T>> generateLists(int itcount, int length, Callable<T> generator,
+            Comparator<T> comparator) throws Exception
+    {
+        List<List<T>> lists = Lists.newArrayList();
+        for (int i=0; i<itcount; ++i) {
+            List<T> l = Lists.newArrayList();
+            for (int j=0; j<length; ++j) {
+                l.add(generator.call());
+            }
+            l.sort(comparator);
+            lists.add(l);
+        }
+        return lists;
+    }
+
+    public <T> void testMergeIterator(Comparator<T> comparator, Reducer<T, Counted<T>> reducer, List<List<T>> lists)
+    {
+        IMergeIterator<T,Counted<T>> tested = MergeIterator.get(closeableIterators(lists), comparator, reducer);
+        IMergeIterator<T,Counted<T>> base = new MergeIteratorPQ<>(closeableIterators(lists), comparator, reducer);
+        // If test fails, try the version below for improved reporting:
+        //   Assert.assertArrayEquals(Iterators.toArray(tested, Counted.class), Iterators.toArray(base, Counted.class));
+        Assert.assertTrue(Iterators.elementsEqual(tested, base));
+        if (!BENCHMARK)
+            return;
+
+        System.out.println();
+        for (int i=0; i<10; ++i) {
+            benchmarkIterator(MergeIterator.get(closeableIterators(lists), comparator, reducer));
+            benchmarkIterator(new MergeIteratorPQ<>(closeableIterators(lists), comparator, reducer));
+        }
+    }
+    
+    public <T> void benchmarkIterator(IMergeIterator<T, Counted<T>> it)
+    {
+        System.out.print("Testing " + it.getClass().getSimpleName() + "... ");
+        long time = System.currentTimeMillis();
+        Counted<T> value = null;
+        while (it.hasNext())
+            value = it.next();
+        time = System.currentTimeMillis() - time;
+        System.out.println("type " + value.item.getClass().getSimpleName() + " time " + time + "ms");
+    }
+
+    public <T> List<CloseableIterator<T>> closeableIterators(List<List<T>> iterators)
+    {
+        return Lists.transform(iterators, new Function<List<T>, CloseableIterator<T>>() {
+
+            @Override
+            public CloseableIterator<T> apply(List<T> arg)
+            {
+                // Go through LinkedHashSet to remove duplicates.
+                return new CLI<T>(new LinkedHashSet<T>(arg).iterator());
+            }
+            
+        });
+    }
+
+    static class Counted<T> {
+        T item;
+        int count;
+        
+        Counted(T item) {
+            this.item = item;
+            count = 0;
+        }
+
+        public boolean equals(Object obj)
+        {
+            if (obj == null || !(obj instanceof Counted))
+                return false;
+            Counted<?> c = (Counted<?>) obj;
+            return Objects.equal(item, c.item) && count == c.count;
+        }
+
+        @Override
+        public String toString()
+        {
+            return item.toString() + "x" + count;
+        }
+    }
+    
+    static class Counter<T> extends Reducer<T, Counted<T>> {
+        Counted<T> current = null;
+
+        @Override
+        public void reduce(T next)
+        {
+            if (current == null)
+                current = new Counted<T>(next);
+            ++current.count;
+        }
+
+        @Override
+        protected void onKeyChange()
+        {
+            current = null;
+        }
+
+        @Override
+        protected Counted<T> getReduced()
+        {
+            return current;
+        }
+    }
+    
+    // closeable list iterator
+    public static class CLI<E> extends AbstractIterator<E> implements CloseableIterator<E>
+    {
+        Iterator<E> iter;
+        boolean closed = false;
+        public CLI(Iterator<E> items)
+        {
+            this.iter = items;
+        }
+
+        protected E computeNext()
+        {
+            if (!iter.hasNext()) return endOfData();
+            return iter.next();
+        }
+
+        public void close()
+        {
+            assert !this.closed;
+            this.closed = true;
+        }
+    }
+
+    // Old MergeIterator implementation for comparison.
+    public class MergeIteratorPQ<In,Out> extends MergeIterator<In,Out> implements IMergeIterator<In, Out>
+    {
+        // a queue for return: all candidates must be open and have at least one item
+        protected final PriorityQueue<Candidate<In>> queue;
+        // a stack of the last consumed candidates, so that we can lazily call 'advance()'
+        // TODO: if we had our own PriorityQueue implementation we could stash items
+        // at the end of its array, so we wouldn't need this storage
+        protected final ArrayDeque<Candidate<In>> candidates;
+        public MergeIteratorPQ(List<? extends Iterator<In>> iters, Comparator<In> comp, Reducer<In, Out> reducer)
+        {
+            super(iters, reducer);
+            this.queue = new PriorityQueue<>(Math.max(1, iters.size()));
+            for (Iterator<In> iter : iters)
+            {
+                Candidate<In> candidate = new Candidate<>(iter, comp);
+                if (candidate.advance() == null)
+                    // was empty
+                    continue;
+                this.queue.add(candidate);
+            }
+            this.candidates = new ArrayDeque<>(queue.size());
+        }
+
+        protected final Out computeNext()
+        {
+            advance();
+            return consume();
+        }
+
+        /** Consume values by sending them to the reducer while they are equal. */
+        protected final Out consume()
+        {
+            reducer.onKeyChange();
+            Candidate<In> candidate = queue.peek();
+            if (candidate == null)
+                return endOfData();
+            do
+            {
+                candidate = queue.poll();
+                candidates.push(candidate);
+                reducer.reduce(candidate.item);
+            }
+            while (queue.peek() != null && queue.peek().compareTo(candidate) == 0);
+            return reducer.getReduced();
+        }
+
+        /** Advance and re-enqueue all items we consumed in the last iteration. */
+        protected final void advance()
+        {
+            Candidate<In> candidate;
+            while ((candidate = candidates.pollFirst()) != null)
+                if (candidate.advance() != null)
+                    queue.add(candidate);
+        }
+    }
+
+}
