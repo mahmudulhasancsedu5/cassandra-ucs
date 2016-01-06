ab7c4c9b469b2dbb1cedbad10a1200bdf46c64b7
Branimir Lambov <branimir.lambov@datastax.com>
2016-01-06 11:22:29 +0200

    Fix ReadCommandTest (CASSANDRA-10951).

--- src/java/org/apache/cassandra/db/ReadCommand.java
+++ src/java/org/apache/cassandra/db/ReadCommand.java
@@ -479,23 +479,31 @@ public abstract class ReadCommand extends MonitorableImpl implements ReadQuery
     {
         protected BaseRowIterator<?> applyToPartition(BaseRowIterator partition)
         {
-            maybeAbort();
+            if (maybeAbort())
+            {
+                partition.close();
+                return null;
+            }
+
             return partition;
         }
 
         protected Row applyToRow(Row row)
         {
-            maybeAbort();
-            return row;
+            return maybeAbort() ? null : row;
         }
 
-        private void maybeAbort()
+        private boolean maybeAbort()
         {
             if (isAborted())
+            {
                 stop();
+                return true;
+            }
 
             if (TEST_ITERATION_DELAY_MILLIS > 0)
                 maybeDelayForTesting();
+            return false;
         }
     }
 
